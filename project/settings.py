26a565a1854d85e6a3c43b3f4c2d3f3b05d9628e
trendyrituals <trendyrituals@gmail.com>
2017-10-30 16:19:26 +0530

    Initial

--- project/settings.py
+++ project/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
     #'/var/www/static/',
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "/static/")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
